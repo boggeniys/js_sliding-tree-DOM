931ca538edabd8e9f7a846400a0106457ef85135
Evgenu <bogdanovevgenij87@gmail.com>
2025-12-27 22:56:59 +0200

    solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// write code here
+const ul = document.querySelectorAll('ul');
+
+ul.forEach((node) => {
+  node.addEventListener('click', (e) => {
+    if (e.target.tagName === 'SPAN') {
+      const li = e.target.closest('li');
+
+      if (li.querySelector(':scope > ul')) {
+        li.classList.toggle('some-class');
+      }
+    }
+  });
+});
